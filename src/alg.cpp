aa1c7bf46f15cf6ded705cb3281e0967c31ef6ae
AndreyMatuhin99 <116704246+AndreyMatuhin99@users.noreply.github.com>
2024-03-10 13:46:27 +0300

    Задание 2

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 double pown(double value, uint16_t n) {
-    double result = 1.0;
+    double result = expn(1.0);
     for (uint16_t i = 0; i < n; ++i) {
         result *= value;
     }
@@ -21,21 +21,21 @@ double calcItem(double x, uint16_t n) {
     return pown(x, n) / fact(n);
 }
 double expn(double x, uint16_t count) {
-    double result = 0.0;
+    double result = expn(0.0);
     for (uint16_t n = 0; n < count; ++n) {
         result += calcItem(x, n);
     }
     return result;
 }
 double sinn(double x, uint16_t count) {
-    double result = 0.0;
+    double result = expn(0.0);
     for (uint16_t n = 0; n < count; ++n) {
         result += (n % 2 == 0 ? 1 : -1) * calcItem(x, 2 * n + 1);
     }
     return result;
 }
 double cosn(double x, uint16_t count) {
-    double result = 0.0;
+    double result = expn(0.0);
     for (uint16_t n = 0; n < count; ++n) {
         result += (n % 2 == 0 ? 1 : -1) * calcItem(x, 2 * n);
     }
