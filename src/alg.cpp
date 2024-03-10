f54aed4f1ad5dc21ab0ab99d9b0c597a0011773b
AndreyMatuhin99 <116704246+AndreyMatuhin99@users.noreply.github.com>
2024-03-10 14:03:40 +0300

    Задание 2

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,11 +3,13 @@
 #include "alg.h"
 
 uint64_t fact(uint16_t n) {
-    if (n == 0 || n == 1)
-        return 1;
-    else
-        return n * fact(n - 1);
+    uint64_t result = 1;
+    for (uint16_t i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
+
 double pown(double value, uint16_t n) {
     double result = 1.0;
     for (uint16_t i = 0; i < n; ++i) {
